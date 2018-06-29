a4fff7e89e35afd2723b2ee712f4a6d98dfdebda
devamitr <dev.amitr@gmail.com>
2018-06-29 08:55:29 -0700

    Merged two sorted Linkedlists

--- src/LinkedListt.java
+++ src/LinkedListt.java
@@ -1,6 +1,12 @@
+import java.util.LinkedList;
+
 public class LinkedListt {
-    public static LinkedListNode head;
-    public static class LinkedListNode {
+    public LinkedListNode head;
+
+    public LinkedListt(){
+        this.head = null;
+    }
+    public class LinkedListNode {
         int data;
         LinkedListNode next;
 
@@ -9,39 +15,39 @@ public class LinkedListt {
             next = null;
         }
     }
-    public static void add_node_end(int d){
-        if(head == null){
-            head = new LinkedListNode(d);
+    public void add_node_end(int d){
+        if(this.head == null){
+            this.head = new LinkedListNode(d);
         }
         else
         {
             LinkedListNode new_node = new LinkedListNode(d);
-            LinkedListNode cursor = head;
+            LinkedListNode cursor = this.head;
             while(cursor.next!=null)
                 cursor = cursor.next;
             cursor.next = new_node;
         }
     }
 
-    public static void add_node_start(int d){
-        if(head == null){
-            head = new LinkedListNode(d);
+    public void add_node_start(int d){
+        if(this.head == null){
+            this.head = new LinkedListNode(d);
         }
         else
         {
             LinkedListNode new_node = new LinkedListNode(d);
-            new_node.next = head;
-            head = new_node;
+            new_node.next = this.head;
+            this.head = new_node;
         }
     }
 
-    public static void add_node_at_index(int d, int index){
-        if(head==null)
-            head = new LinkedListNode(d);
+    public void add_node_at_index(int d, int index){
+        if(this.head==null)
+            this.head = new LinkedListNode(d);
         else{
             int i=0;
             LinkedListNode new_node = new LinkedListNode(d);
-            LinkedListNode cursor = head;
+            LinkedListNode cursor = this.head;
             while(cursor.next!=null && i<index-1) {
                 cursor = cursor.next;
                 i++;
@@ -52,10 +58,10 @@ public class LinkedListt {
         }
     }
 
-    public static void delete_node(int d){
-        LinkedListNode cursor = head;
-        if(head.data == d) {
-            head = head.next;
+    public void delete_node(int d){
+        LinkedListNode cursor = this.head;
+        if(this.head.data == d) {
+            this.head = this.head.next;
             return;
         }
         while(cursor.next!=null) {
@@ -68,9 +74,9 @@ public class LinkedListt {
         }
     }
 
-    public static void print_linked_list()
+    public void print_linked_list()
     {
-        LinkedListNode cursor = head;
+        LinkedListNode cursor = this.head;
         System.out.println("******************************");
         while(cursor!=null){
             System.out.println(cursor.data);
@@ -78,29 +84,29 @@ public class LinkedListt {
         }
     }
 
-    public static void reverse_linked_list()
+    public void reverse_linked_list()
     {
-        LinkedListNode prev = null, next = null,cursor = head;
+        LinkedListNode prev = null, next = null,cursor = this.head;
         while(cursor!=null){
             next = cursor.next;
             cursor.next = prev;
             prev = cursor;
             cursor = next;
         }
-        head = prev;
+        this.head = prev;
     }
-    public static boolean swap_nodes(int x, int y) {
-        LinkedListNode xnode = null, ynode = null, cursor = head;
+    public boolean swap_nodes(int x, int y) {
+        LinkedListNode xnode = null, ynode = null, cursor = this.head;
         LinkedListNode xprev = null, yprev = null;
         Boolean x_head = false, y_head = false;
-        if (x == head.data) {
+        if (x == this.head.data) {
             xprev = null;
-            xnode = head;
+            xnode = this.head;
             x_head = true;
         }
-        else if (y == head.data) {
+        else if (y == this.head.data) {
             yprev = null;
-            ynode = head;
+            ynode = this.head;
             y_head = true;
         }
             while (cursor.next != null && (xnode == null || ynode == null)) {
@@ -124,13 +130,31 @@ public class LinkedListt {
             ynode.next = xnode.next;
             xnode.next = temp;
             if(x_head)
-                head = ynode;
+                this.head = ynode;
             if(y_head)
-                head = xnode;
+                this.head = xnode;
             return true;
         } else {
             return false;
         }
 
+    }
+
+    public LinkedListNode merge_sorted_linkedlist(LinkedListNode ll1, LinkedListNode ll2)
+    {
+        LinkedListNode res = null;
+        if(ll1 == null)
+            return ll2;
+        if(ll2 == null)
+            return ll1;
+        if(ll1.data<=ll2.data){
+            res = ll1;
+            res.next = merge_sorted_linkedlist(ll1.next, ll2);
+        }
+        else{
+            res = ll2;
+            res.next = merge_sorted_linkedlist(ll1, ll2.next);
+        }
+        return res;
     }
 }
--- src/implementation.java
+++ src/implementation.java
@@ -6,10 +6,18 @@ public class implementation {
     public static void linked_list_implementation(){
         LinkedListt LinkedList1 = new LinkedListt();
         System.out.println("please insert number of data for the linkedlist:");
-        int[] inp_arr = {1,2,3,4,5,6};
+        int[] inp_arr = {1,3,5,6,10};
         for(int i=0;i<inp_arr.length;i++)
         {
             LinkedList1.add_node_end(inp_arr[i]);
+        }
+
+        LinkedListt LinkedList2 = new LinkedListt();
+        System.out.println("please insert number of data for the linkedlist:");
+        int[] inp_arr2 = {2,4,6,8,11};
+        for(int i=0;i<inp_arr2.length;i++)
+        {
+            LinkedList2.add_node_end(inp_arr2[i]);
         }
         LinkedList1.add_node_start(10);
         LinkedList1.add_node_at_index(20,3);
@@ -20,6 +28,11 @@ public class implementation {
         LinkedList1.print_linked_list();
         LinkedList1.swap_nodes(6,10);
         LinkedList1.print_linked_list();
+        LinkedList2.print_linked_list();
+
+        LinkedListt res = new LinkedListt();
+        res.head = LinkedList1.merge_sorted_linkedlist(LinkedList1.head, LinkedList2.head);
+        res.print_linked_list();
     }
 
 
